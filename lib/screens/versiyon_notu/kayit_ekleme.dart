28c05294ee39e8908d74fb3402a01df56986a9e8
btnahmet <ahmetbutun27@gmail.com>
2024-10-10 12:21:05 +0300

    Versiyon Not Commit

--- lib/screens/versiyon_notu/kayit_ekleme.dart
+++ lib/screens/versiyon_notu/kayit_ekleme.dart
@@ -1,257 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class KayitEkleme8 extends StatefulWidget {
-//   const KayitEkleme8({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   State<KayitEkleme8> createState() => _KayitEklemeState();
-// }
-
-// class _KayitEklemeState extends State<KayitEkleme8> {
-//   String? selectedValue;
-//   List<String> items = ['Seçenek 1', 'Seçenek 2', 'Seçenek 3', 'Seçenek 4'];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         backgroundColor: const Color.fromARGB(255, 233, 233, 231),
-//         appBar: AppBar(
-//           backgroundColor: const Color.fromARGB(255, 224, 224, 203),
-//           title: const Text(
-//             'NOT EKLEME',
-//             style: TextStyle(fontWeight: FontWeight.bold),
-//           ),
-//         ),
-//         body: Padding(
-//           padding: EdgeInsets.all(16),
-//           child: Column(
-//             children: [
-//               Row(
-//                 children: [Text("Önem Derecesi")],
-//               ),
-//               Row(
-//                 children: [
-//                   DropdownButton<String>(
-//                     value: selectedValue,
-//                     hint: Text('Önem derecesini seçin'), // Varsayılan gösterilen text
-//                     items: items.map((String item) {
-//                       return DropdownMenuItem<String>(
-//                         value: item,
-//                         child: Text(item),
-//                       );
-//                     }).toList(),
-//                     onChanged: (String? newValue) {
-//                       setState(() {
-//                         selectedValue = newValue;
-//                       });
-//                     },
-//                   ),
-//                 ],
-//               ),
-//               Padding(
-//                 padding: EdgeInsets.all(8),
-//                 child: Row(
-//                   children: [
-//                     Text(
-//                       "Başlık",
-//                       style: TextStyle(
-//                         fontSize: 20,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//               Padding(
-//                 padding: EdgeInsets.all(0),
-//                 child: Row(
-//                   children: [Expanded(child: TextFormField())],
-//                 ),
-//               ),
-//               Row(
-//                 children: [Text("Not İçeriği")],
-//               ),
-//               Row(
-//                 children: [Expanded(child: TextFormField())],
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// import 'package:flutter/material.dart';
-
-// class KayitEkleme8 extends StatefulWidget {
-//   const KayitEkleme8({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   State<KayitEkleme8> createState() => _KayitEklemeState();
-// }
-
-// class _KayitEklemeState extends State<KayitEkleme8> {
-//   String? selectedValue;
-//   List<String> items = ['Önemsiz', 'Önemli', 'Çok Önemli', 'Kritik Önem!'];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         backgroundColor: const Color.fromARGB(255, 233, 233, 231),
-//         appBar: AppBar(
-//           backgroundColor: const Color.fromARGB(255, 224, 224, 203),
-//           title: const Text(
-//             'NOT EKLEME',
-//             style: TextStyle(
-//               fontWeight: FontWeight.bold,
-//               fontSize: 24, // Başlık font boyutu
-//             ),
-//           ),
-//         ),
-//         body: SingleChildScrollView(
-//           child: Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: Column(
-//               crossAxisAlignment:
-//                   CrossAxisAlignment.start, // Elemanları sola hizala
-//               children: [
-//                 Padding(
-//                   padding: const EdgeInsets.fromLTRB(0, 0, 0, 12),
-//                   child: Row(
-//                     children: const [
-//                       Text(
-//                         "Önem Derecesi",
-//                         style: TextStyle(
-//                           fontSize: 18, // Önem derecesi font boyutu
-//                           fontWeight: FontWeight.bold, // Orta kalınlıkta
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(bottom: 12.0),
-//                   child: Row(
-//                     children: [
-//                       Container(
-//                         decoration: BoxDecoration(
-//                           color: Color.fromARGB(
-//                               255, 223, 223, 213), // Arka plan rengi
-//                           borderRadius: BorderRadius.circular(
-//                               8), // Kenarların yuvarlatılması (opsiyonel)
-//                         ),
-//                         padding: const EdgeInsets.symmetric(
-//                             horizontal: 12), // İçerideki boşluk (opsiyonel)
-//                         child: DropdownButton<String>(
-//                           value: selectedValue,
-//                           hint: const Text(
-//                             'Önem derecesini seçin',
-//                             style: TextStyle(
-//                               fontSize: 16,
-//                               fontWeight: FontWeight.w900,
-//                             ), // Dropdown font boyutu
-//                           ),
-//                           items: items.map((String item) {
-//                             return DropdownMenuItem<String>(
-//                               value: item,
-//                               child: Text(
-//                                 item,
-//                                 style: const TextStyle(
-//                                     fontSize: 16), // Dropdown menü itemleri
-//                               ),
-//                             );
-//                           }).toList(),
-//                           onChanged: (String? newValue) {
-//                             setState(() {
-//                               selectedValue = newValue;
-//                             });
-//                           },
-//                           dropdownColor: const Color.fromARGB(255, 216, 222,
-//                               224), // Açılan menüdeki arka plan rengi
-//                           underline: Container(), // Alt çizgiyi kaldırmak için
-//                         ),
-//                       )
-//                     ],
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(bottom: 8.0),
-//                   child: Row(
-//                     children: const [
-//                       Text(
-//                         "Başlık",
-//                         style: TextStyle(
-//                           fontSize: 20,
-//                           fontWeight: FontWeight.bold, // Kalın başlık
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(bottom: 16.0),
-//                   child: Row(
-//                     children: [
-//                       Expanded(
-//                         child: TextFormField(
-//                           decoration: InputDecoration(
-//                             border: OutlineInputBorder(),
-//                             hintText: 'Başlık girin..', // Placeholder ekleme
-//                           ),
-//                           style: TextStyle(fontSize: 16), // Input font boyutu
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: const EdgeInsets.only(bottom: 8.0),
-//                   child: Row(
-//                     children: const [
-//                       Text(
-//                         "Not İçeriği",
-//                         style: TextStyle(
-//                           fontSize: 18, // Not içeriği başlığı font boyutu
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Row(
-//                   children: [
-//                     Expanded(
-//                       child: TextFormField(
-//                         maxLines: 7, // Çok satırlı text field
-//                         decoration: InputDecoration(
-//                           border: OutlineInputBorder(),
-//                           hintText: 'Notunuzu girin...', // Placeholder ekleme
-//                         ),
-//                         style: TextStyle(fontSize: 16), // Input font boyutu
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//                 Padding(
-//                   padding: EdgeInsets.fromLTRB(105,20 , 0, 0),
-//                   child: Row(
-//                     children: [
-//                       ElevatedButton(onPressed: () {}, child: Text("Notu Kaydet"))
-//                     ],
-//                   ),
-//                 )
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class KayitEkleme8 extends StatefulWidget {
@@ -266,6 +12,7 @@ class _KayitEklemeState extends State<KayitEkleme8> {
   String? selectedValue;
   TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
+  TextEditingController personController = TextEditingController();
   List<String> items = ['Önemsiz', 'Önemli', 'Çok Önemli', 'Kritik Önem!'];
 
   @override
@@ -279,7 +26,7 @@ class _KayitEklemeState extends State<KayitEkleme8> {
             'NOT EKLEME',
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              fontSize: 24, // Başlık font boyutu
+              fontSize: 24,
             ),
           ),
         ),
@@ -287,15 +34,15 @@ class _KayitEklemeState extends State<KayitEkleme8> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Elemanları sola hizala
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Padding(
                   padding: EdgeInsets.fromLTRB(0, 0, 0, 12),
                   child: Text(
                     "Önem Derecesi",
                     style: TextStyle(
-                      fontSize: 18, // Önem derecesi font boyutu
-                      fontWeight: FontWeight.bold, // Orta kalınlıkta
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
@@ -334,7 +81,7 @@ class _KayitEklemeState extends State<KayitEkleme8> {
                     "Başlık",
                     style: TextStyle(
                       fontSize: 20,
-                      fontWeight: FontWeight.bold, // Kalın başlık
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
@@ -368,14 +115,35 @@ class _KayitEklemeState extends State<KayitEkleme8> {
                   ),
                   style: const TextStyle(fontSize: 16),
                 ),
+                 const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    "Notu Ekleyen",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: TextFormField(
+                     controller: personController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Notu Ekleyen...'
+                    ),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(105, 20, 0, 0),
                   child: ElevatedButton(
                     onPressed: () {
-                      // Kaydet ve geri dön
                       Navigator.pop(context, {
                         'title': titleController.text,
                         'content': contentController.text,
+                        'person': personController.text,
                         'importance': selectedValue ?? 'Belirtilmedi',
                       });
                     },
--- lib/screens/versiyon_notu/versiyon_notu.dart
+++ lib/screens/versiyon_notu/versiyon_notu.dart
@@ -1,77 +1,5 @@
-// // import 'package:flutter/material.dart';
-// // import 'package:mobil_app/screens/versiyon_notu/kayit_ekleme.dart';
-
-// // class VersiyonNotu extends StatefulWidget {
-// //   const VersiyonNotu({super.key, required this.title});
-// //   final String title;
-
-// //   @override
-// //   State<VersiyonNotu> createState() => _VersiyonNotuState();
-// // }
-
-// // class _VersiyonNotuState extends State<VersiyonNotu> {
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     double deviceWidth = MediaQuery.of(context).size.width;
-// //     double deviceHeight = MediaQuery.of(context).size.height;
-// //     return SafeArea(
-// //       child: Scaffold(
-// //         backgroundColor: const Color.fromARGB(255, 233, 233, 228),
-// //         appBar: AppBar(
-// //           backgroundColor: const Color.fromARGB(255, 219, 219, 195),
-// //           title: Container(
-// //             child: const Row(
-// //               mainAxisSize: MainAxisSize.min,
-// //               children: [
-// //                 Text(
-// //                   "NOTLARIM",
-// //                   style: TextStyle(
-// //                     fontWeight: FontWeight.bold,
-// //                   ),
-// //                 )
-// //               ],
-// //             ),
-// //           ),
-// //         ),
-// //         body: Padding(
-// //           //padding: EdgeInsets.symmetric(horizontal: deviceWidth/30),
-// //           padding: EdgeInsets.fromLTRB(
-// //               deviceWidth / 18, deviceHeight / 18, deviceWidth / 10, 0),
-// //           child: Row(
-// //             children: [
-// //               ElevatedButton(
-// //                 onPressed: () {
-// //                   Navigator.push(
-// //                     context,
-// //                     MaterialPageRoute(
-// //                       builder: (context) => const KayitEkleme8(title: ""),
-// //                     ),
-// //                   );
-// //                 },
-// //                 child: Row(
-// //                   children: [
-// //                     Image.asset(
-// //                       "lib/assets/images/plus_icon.png",
-// //                       height: deviceHeight * 0.03,
-// //                     ),
-// //                     SizedBox(width: deviceWidth * 0.03),
-// //                     const Text(
-// //                       "Not Ekle",
-// //                       style: TextStyle(fontSize: 20, color: Colors.black),
-// //                     ),
-// //                   ],
-// //                 ),
-// //               ),
-// //             ],
-// //           ),
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
-
 // import 'package:flutter/material.dart';
-// import 'kayit_ekleme.dart';
+// import 'package:mobil_app/screens/versiyon_notu/kayit_ekleme.dart';
 
 // class VersiyonNotu extends StatefulWidget {
 //   const VersiyonNotu({super.key, required this.title});
@@ -82,14 +10,39 @@
 // }
 
 // class _VersiyonNotuState extends State<VersiyonNotu> {
-//   // This list will store notes added from the second page
 //   List<Map<String, String>> notes = [];
 
-//   // This method will be called when returning from the KayitEkleme8 page
 //   void addNote(Map<String, String> newNote) {
 //     setState(() {
 //       notes.add(newNote);
+//       notes.sort(
+//           (a, b) => compareImportance(a['importance']!, b['importance']!));
 //     });
+//   }
+
+//   int compareImportance(String a, String b) {
+//     const importanceLevels = {
+//       'Kritik Önem!': 0,
+//       'Çok Önemli': 1,
+//       'Önemli': 2,
+//       'Önemsiz': 3,
+//     };
+//     return importanceLevels[a]!.compareTo(importanceLevels[b]!);
+//   }
+
+//   Color getColorForImportance(String importance) {
+//     switch (importance) {
+//       case 'Kritik Önem!':
+//         return Color.fromARGB(255, 240, 3, 3); 
+//       case 'Çok Önemli':
+//         return Colors.orangeAccent;
+//       case 'Önemli':
+//         return Colors.yellowAccent;
+//       case 'Önemsiz':
+//         return Color.fromARGB(255, 33, 202, 120);
+//       default:
+//         return Colors.white;
+//     }
 //   }
 
 //   @override
@@ -114,44 +67,16 @@
 //               deviceWidth / 18, deviceHeight / 18, deviceWidth / 10, 0),
 //           child: Column(
 //             children: [
-//               ElevatedButton(
-//                 onPressed: () async {
-//                   // Navigate to KayitEkleme8 and wait for a result (new note)
-//                   final result = await Navigator.push(
-//                     context,
-//                     MaterialPageRoute(
-//                       builder: (context) => const KayitEkleme8(title: ""),
-//                     ),
-//                   );
-
-//                   // If a note is returned, add it to the list
-//                   if (result != null && result is Map<String, String>) {
-//                     addNote(result);
-//                   }
-//                 },
-//                 child: Row(
-//                   children: [
-//                     Image.asset(
-//                       "lib/assets/images/plus_icon.png",
-//                       height: deviceHeight * 0.03,
-//                     ),
-//                     SizedBox(width: deviceWidth * 0.03),
-//                     const Text(
-//                       "Not Ekle",
-//                       style: TextStyle(fontSize: 20, color: Colors.black),
-//                     ),
-//                   ],
-//                 ),
-//               ),
 //               Expanded(
 //                 child: ListView.builder(
 //                   itemCount: notes.length,
 //                   itemBuilder: (context, index) {
 //                     final note = notes[index];
+//                     final importance = note['importance'] ?? '';
 //                     return Padding(
 //                       padding: const EdgeInsets.symmetric(vertical: 5.0),
 //                       child: Card(
-//                         color: const Color.fromARGB(255, 233, 233, 228),
+//                         color: getColorForImportance(importance),
 //                         elevation: 3,
 //                         child: ExpansionTile(
 //                           title: Text(
@@ -162,10 +87,10 @@
 //                             ),
 //                           ),
 //                           subtitle: Text(
-//                             note['importance'] ?? '',
+//                             importance,
 //                             style: const TextStyle(
 //                               fontSize: 16,
-//                               color: Colors.grey,
+//                               color: Color.fromARGB(255, 0, 0, 0),
 //                             ),
 //                           ),
 //                           children: [
@@ -178,9 +103,37 @@
 //                             ),
 //                           ],
 //                         ),
+
 //                       ),
 //                     );
 //                   },
+//                 ),
+//               ),
+//               Padding(
+//                 padding: EdgeInsets.only(bottom: deviceHeight * 0.1),
+//                 child: ElevatedButton(
+//                   onPressed: () async {
+//                     final result = await Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => const KayitEkleme8(title: ""),
+//                       ),
+//                     );
+
+//                     if (result != null && result is Map<String, String>) {
+//                       addNote(result);
+//                     }
+//                   },
+//                   child: Row(
+//                     children: [
+//                       Icon(Icons.add, size: deviceHeight * 0.03),
+//                       SizedBox(width: deviceWidth * 0.03),
+//                       const Text(
+//                         "Not Ekle",
+//                         style: TextStyle(fontSize: 20, color: Colors.black),
+//                       ),
+//                     ],
+//                   ),
 //                 ),
 //               ),
 //             ],
@@ -190,6 +143,7 @@
 //     );
 //   }
 // }
+// versiyon_notu.dart
 import 'package:flutter/material.dart';
 import 'kayit_ekleme.dart';
 
@@ -202,14 +156,39 @@ class VersiyonNotu extends StatefulWidget {
 }
 
 class _VersiyonNotuState extends State<VersiyonNotu> {
-  // This list will store notes added from the second page
   List<Map<String, String>> notes = [];
 
-  // This method will be called when returning from the KayitEkleme8 page
   void addNote(Map<String, String> newNote) {
     setState(() {
       notes.add(newNote);
+      notes.sort(
+          (a, b) => compareImportance(a['importance']!, b['importance']!));
     });
+  }
+
+  int compareImportance(String a, String b) {
+    const importanceLevels = {
+      'Kritik Önem!': 0,
+      'Çok Önemli': 1,
+      'Önemli': 2,
+      'Önemsiz': 3,
+    };
+    return importanceLevels[a]!.compareTo(importanceLevels[b]!);
+  }
+
+  Color getColorForImportance(String importance) {
+    switch (importance) {
+      case 'Kritik Önem!':
+        return const Color.fromARGB(255, 240, 3, 3);
+      case 'Çok Önemli':
+        return Colors.orangeAccent;
+      case 'Önemli':
+        return Colors.yellowAccent;
+      case 'Önemsiz':
+        return const Color.fromARGB(255, 33, 202, 120);
+      default:
+        return Colors.white;
+    }
   }
 
   @override
@@ -239,24 +218,42 @@ class _VersiyonNotuState extends State<VersiyonNotu> {
                   itemCount: notes.length,
                   itemBuilder: (context, index) {
                     final note = notes[index];
+                    final importance = note['importance'] ?? '';
                     return Padding(
                       padding: const EdgeInsets.symmetric(vertical: 5.0),
                       child: Card(
-                        color: const Color.fromARGB(255, 233, 233, 228),
+                        color: getColorForImportance(importance),
                         elevation: 3,
                         child: ExpansionTile(
-                          title: Text(
-                            note['title'] ?? '',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
+                          title: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  note['title'] ?? '',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                child: Text(
+                                  note['person'] ?? '',
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    // fontStyle: FontStyle.,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                           subtitle: Text(
-                            note['importance'] ?? '',
+                            importance,
                             style: const TextStyle(
                               fontSize: 16,
-                              color: Colors.grey,
+                              color: Colors.black,
                             ),
                           ),
                           children: [
@@ -275,32 +272,30 @@ class _VersiyonNotuState extends State<VersiyonNotu> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(bottom: deviceHeight*0.1),
+                padding: EdgeInsets.only(bottom: deviceHeight * 0.1),
                 child: ElevatedButton(
                   onPressed: () async {
-                    // Navigate to KayitEkleme8 and wait for a result (new note)
                     final result = await Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => const KayitEkleme8(title: ""),
                       ),
                     );
-                
-                    // If a note is returned, add it to the list
+
                     if (result != null && result is Map<String, String>) {
                       addNote(result);
                     }
                   },
                   child: Row(
                     children: [
-                      Image.asset(
-                        "lib/assets/images/plus_icon.png",
-                        height: deviceHeight * 0.03,
-                      ),
+                      Icon(Icons.add, size: deviceHeight * 0.03),
                       SizedBox(width: deviceWidth * 0.03),
                       const Text(
-                        "Not Ekle",
-                        style: TextStyle(fontSize: 20, color: Colors.black),
+                        'Yeni Not Ekle',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ],
                   ),
